d098683656ba1175211d0f57d99b5421a981f265
Renee Storey-Lussier <reneesl@Renees-MBP.royaume.com>
2022-10-24 12:42:46 -0400

    commit set up

---
+++ routes/index.js
@@ -0,0 +1,28 @@
+var router = require("express").Router();
+// new code below
+const passport = require("passport");
+
+// The root route renders our only view
+router.get("/", function (req, res) {
+  res.redirect("/students");
+});
+// Google OAuth login route
+router.get(
+  "/auth/google",
+  passport.authenticate("google", { scope: ["profile", "email"] })
+);
+// Google OAuth callback route
+router.get(
+  "/oauth2callback",
+  passport.authenticate("google", {
+    successRedirect: "/students",
+    failureRedirect: "/students",
+  })
+);
+// OAuth logout route
+router.get("/logout", function (req, res) {
+  req.logout();
+  res.redirect("/students");
+});
+
+module.exports = router;
