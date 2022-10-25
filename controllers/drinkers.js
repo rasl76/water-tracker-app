e0cad7efbdc7aa9faf5e975e5b2a4ebd7c8e3043
Renee Storey-Lussier <reneesl@Renees-MBP.royaume.com>
2022-10-25 16:55:23 -0400

    styling updates

--- controllers/drinkers.js
+++ controllers/drinkers.js
@@ -19,7 +19,7 @@ function index(req, res, next) {
 }
 
 function addWater(req, res, next) {
-  req.user.facts.push(req.body);
+  req.user.waters.push(req.body);
   req.user.save(function (err) {
     res.redirect("/drinkers");
   });
